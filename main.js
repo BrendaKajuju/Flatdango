9e02f5fc1321e5f18e402508dce71d4765814f36
Brenda <brendakajuju777@gmail.com>
2023-04-04 14:09:54 +0300

    Basin JS format added

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 const baseURL = `https://my-json-server.typicode.com/BrendaKajuju/Flatdango/films`
 const nowPLayingUl = document.getElementById('now-playing')
-/**
- * Title
- * Overview
- * Votes Average
- * Poster Path
- * ID
- */
+// Core deliverable 1: See the movie details when page loads
 function getNowPlayingMovies() {
     fetch(baseURL)
     .then (response => response.json())
@@ -29,19 +23,58 @@ function getNowPlayingMovies() {
     moviesArr.map(displayMovies)})
   };
 
-function displayMovies(movieObj) {
+  function displayMovies(movieObj) {
     let HTMLTemplate = `
-    <li class = "col-md-5 mx-4>
-    <div class="card" style="width: 25rem;">
-    <img src="${movieObj.image}" class="card-img-top" alt="${movieObj.title}">
-    <div class="card-body">
-    <h5 class="card-title">"${movieObj.title}"</h5>
-    <p class="card-text">${movieObj.showtime}</p>
-    <p class="card-text">${movieObj.runtime} minutes</p>
-    <p class="card-text">${movieObj.description}</p>
-    </div>
-  </div>
-</li>`;
-    nowPLayingUl.innerHTML+=HTMLTemplate;   
-}
+      <li class="col-md-10 mx-7">
+        <div class="card" style="width: 30rem;">
+          <img src="${movieObj.image}" class="card-img-top" alt="${movieObj.title}">
+          <div class="card-body">
+            <h5 class="card-title">${movieObj.title}</h5>
+            <p class="card-text">${movieObj.showtime}</p>
+            <p class="card-text">${movieObj.runtime} minutes</p>
+            <p class="card-text">${movieObj.description}</p>
+            <p class="card-text">Available seats: <span id="available-seats-${movieObj.id}">${movieObj.capacity}</span></p>
+            <p class="card-text">Tickets Sold: <span id="movie-tickets-${movieObj.id}">${movieObj.tickets_sold}</span></p>
+            <button type="button" class="btn btn-primary buy-ticket-btn" data-id="${movieObj.id}">Buy Tickets</button>
+          </div>
+        </div>
+      </li>
+    `;
+    nowPLayingUl.innerHTML += HTMLTemplate;
+  
+    const buyTicketBtn = document.querySelector(`button[data-id="${movieObj.id}"]`);
+    const availableSeats = document.getElementById(`available-seats-${movieObj.id}`);
+    const movieTickets = document.getElementById(`movie-tickets-${movieObj.id}`);
+  
+    buyTicketBtn.addEventListener('click', () => {
+      if (parseInt(availableSeats.textContent) === 0) {
+        alert(`Sorry, ${movieObj.title} is sold out.`);
+      } else {
+        availableSeats.textContent = parseInt(availableSeats.textContent) - 1;
+        movieTickets.textContent = parseInt(movieTickets.textContent) + 1;
+  
+        // Core Deliverable 3: Update the database
+        const updatedMovieObj = {
+          ...movieObj,
+          capacity: parseInt(availableSeats.textContent),
+          tickets_sold: parseInt(movieTickets.textContent)
+        };
+  
+        fetch(`${baseURL}/${movieObj.id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(updatedMovieObj)
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+      }
+    });
+  }
+  
+
+
+
 getNowPlayingMovies();
